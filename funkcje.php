4877075493abae4db947709eda917362b4c72438
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2016-12-10 15:37:20 +0100

    Pliki z funkcjami

---
+++ funkcje.php
@@ -0,0 +1,4 @@
+<?php
+function dodaj($liczba1, $liczba2=4) {
+  return $liczba1+$liczba2;
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'funkcje.php';
+
+$wynik = dodaj(2, 5);
+echo $wynik; // 7
+
+echo '<br>';
+
+echo dodaj(2); // 6
